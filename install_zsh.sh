325626afec45c7582765e1e1243c043cb0124673
Phu Dinh <55571245+phudinhtruongk18@users.noreply.github.com>
2023-05-07 10:57:46 +0700

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -6,12 +6,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install Zsh Autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Enable Zsh Autosuggestions
-source ~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
 # Install Zsh Syntax Highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Enable Zsh Syntax Highlighting
-source ~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
