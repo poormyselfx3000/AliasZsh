af5039d40f0de410133a71f2662b3eb5c560f4c2
Phu Dinh <55571245+phudinhtruongk18@users.noreply.github.com>
2023-10-07 11:58:16 +0700

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,11 +2,10 @@
 
 sudo apt-get install zsh -y
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh /dev/stdin --unattended
 
 # Install Zsh Autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
 # Install Zsh Syntax Highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
